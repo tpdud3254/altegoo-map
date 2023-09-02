6744e331e9ba32d2ef1e757e070c4aca7bf1bd6a
seyoung Ko <tpdud3256@gmail.com>
2023-09-03 04:30:34 +0900

    인증 페이지 추가

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import Map from "./screens/Map";
 import Map2 from "./screens/Map2";
 import Payment from "./screens/Payment";
+import Certification from "./screens/Certification";
 
 function App() {
     return (
         <div>
             <Routes>
+                <Route path="/certification" element={<Certification />} />
                 <Route path="/map" element={<Map />} />
                 <Route path="/map2" element={<Map2 />} />
                 <Route path="/payment" element={<Payment />} />
